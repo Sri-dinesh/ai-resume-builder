b9c642a3f7b946e6fd4c36fea520853a3ea61ce1
atomic_coding <101187384+Sri-dinesh@users.noreply.github.com>
2025-02-17 14:52:19 +0530

    lint config & createresume button fix

--- src/app/(main)/resumes/ResumesClient.tsx
+++ src/app/(main)/resumes/ResumesClient.tsx
@@ -17,14 +17,19 @@ export default function ResumesClient({
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <CreateResumeButton canCreate={subscriptionLevel && totalCount} />
+      <CreateResumeButton canCreate={!!(subscriptionLevel && totalCount)} />
+      {/* <CreateResumeButton canCreate={subscriptionLevel && totalCount} /> */}
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your resumes</h1>
         <p>Total: {totalCount}</p>
       </div>
       <div className="flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4">
         {resumes.map((resume) => (
-          <ResumeItem key={resume.id} resume={resume} contentRef={contentRef} />
+          <ResumeItem
+            key={resume.id}
+            resume={resume}
+            contentRef={contentRef}
+          />
         ))}
       </div>
     </main>
